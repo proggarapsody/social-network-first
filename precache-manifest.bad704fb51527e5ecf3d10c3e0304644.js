ead83b3cba32aad9814d02f93f787129cebb227c
Aleksey <76034634+alekSSeyKlimenko@users.noreply.github.com>
2021-07-09 16:57:07 +0300

    Updates

---
+++ precache-manifest.bad704fb51527e5ecf3d10c3e0304644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05e414cb8a6aefdf7de1",
+    "url": "/social-network-first/static/css/main.8e28980e.chunk.css"
+  },
+  {
+    "revision": "05e414cb8a6aefdf7de1",
+    "url": "/social-network-first/static/js/main.05e414cb.chunk.js"
+  },
+  {
+    "revision": "b3938764d0e110855f19",
+    "url": "/social-network-first/static/js/1.b3938764.chunk.js"
+  },
+  {
+    "revision": "ff9a48e1a6de93ea8ce2",
+    "url": "/social-network-first/static/js/runtime~main.ff9a48e1.js"
+  },
+  {
+    "revision": "305f3d60f83d70f17998f388b0712fa0",
+    "url": "/social-network-first/static/media/preloader.305f3d60.svg"
+  },
+  {
+    "revision": "dfdfd47d57df823b0acb9b92f63c9afa",
+    "url": "/social-network-first/index.html"
+  }
+];
